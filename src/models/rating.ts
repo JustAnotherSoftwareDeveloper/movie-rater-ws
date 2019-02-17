d9667c8444ed81de7b1cc7ffcd5be45122b4de24
Michael <michael.sila.jobs@gmail.com>
2019-02-16 17:57:12 -0700

    Deleting Unused Files

--- src/models/rating.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from './category';
-import { Movie } from './movie';
-import { UserInfo } from './user-info';
-export interface Rating {
-    user: UserInfo;
-    movie: Movie
-    category: Category;
-    score: 0 | 1 | 2 | 3 | 4 | 5
-}
--- src/models/user-info.ts
+++
@@ -1,7 +0,0 @@
-
-
-export interface UserInfo {
-    username: string;
-    userId: number;
-
-}
